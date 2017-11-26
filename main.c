76df91031d23f0204d4eca2e9fee08d37f6a7640
Alexey <darknessest>
2017-11-26 12:00:26 +0800

    问题 F: [C语言入门题] 词频统计

--- main.c
+++ main.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include <string.h>
 
-_Bool prime(unsigned long long a)
+char unique[](char *strg)
 {
-    for (unsigned long long i = 2; i < sqrt(a); i++) {
-        if (a%i == 0)
-            return 0;
-        else
-            return 1;
+    char temp[1000];
+    for (int i = 0; i < strlen(strg); i++) {
+        if (strg[i] != ' ') {
+            temp[i] = strg[i];
+        }
     }
+    return temp;
 }
 
 int main()
 {
-    unsigned long long x = 0;
-    scanf("%llu", &x);
-
-    if (x > 3 && prime(x) == 1)
-        printf("%llu is primary", x);
-    else
-        printf("error", x);
+    char string[10000];
+    while (gets(string) != EOF) {
+        printf("%s", unique(string));
+    }
 }
