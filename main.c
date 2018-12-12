42e79f77018c3081c074f326be71a4b235db68dc
Darknessest <darknessest>
2018-12-12 10:12:12 +0800

    Dutch Flag problem

--- main.c
+++ main.c
@@ -1,139 +1,45 @@
-//6_2
+//7_1
 /*
-哈希表设计。为班级30个人的姓氏(单字姓)设计一个哈希表，假设姓氏用汉语拼音表示。
-要求用除留取余法构造哈希函数，用线性探测再散列法处理冲突，平均查找长度的上限为2。
+荷兰国旗问题：设有一个仅由红、白、蓝三种颜色的条块组成的序列。
+ 试设计一个时间复杂度为O(n)的算法，使得这些条块按红、白、蓝的顺序排好，即排成荷兰国旗图案。
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <memory.h>
-#include <stdbool.h>
 
-#define phi 0.6180339887
-#define ULL unsigned long long
+#define SIZE 100
 
-typedef struct hn {
-  ULL hash;
-  char name[25];
-  struct hn *next;
-} HN; //hashed names
-void swap_HN(HN *i, HN *j) {
-    HN t;
-    t = *i;
-    *i = *j;
-    *j = t;
-}
-int compare(const void *s1, const void *s2) {
-    HN *e1 = (HN *) s1;
-    HN *e2 = (HN *) s2;
-    //"casting" to int
-    return e1->hash/1000000 - e2->hash/1000000;
-}
-ULL hashing(const char *name) {
-    ULL x = 0;
-    int sum = 0, iter = 2;
-    for (int i = 0; name[i] != '\0'; ++i) {
-        if (name[i] >= 'a' && name[i] <= 'z') {
-            sum += (int) ((name[i]/phi)*100 + name[i]*i - 10000);
-        } else if (name[i] == ' ') {
-            x += sum*(ULL) pow(100000, iter--);
-            sum = 0;
-        } else if (name[i] == '\n') {
-            x += sum*(ULL) pow(100000, iter);
-            iter = 2;
-            break;
-        }
+void countSort(int *array, const int size) {
+    int counter[3];
+    counter[0] = 0;
+    counter[1] = 0;
+    counter[2] = 0;
+    //counting numbers
+    for (int i = 0; i < size; ++i) {
+        if (array[i] == 0)
+            ++counter[0];
+        if (array[i] == 1)
+            ++counter[1];
+        if (array[i] == 2)
+            ++counter[2];
     }
-    x += sum*(ULL) pow(100000, iter);
-//    printf("#%s: %llu\n", name, x);
-    return x;
-}
-void print_hn(const HN *array, const int size) {
-    for (int k = 0; k < size; ++k) {
-        printf("%s : %llu", array[k].name, array[k].hash);
-        if (array[k].next) {
-            printf(" -> %s : %llu", array[k].next->name, array[k].next->hash);
-            if (array[k].next->next)
-                printf(" -> %s : %llu", array[k].next->next->name, array[k].next->next->hash);
+//    printf("#%d\t%d\t%d\n", counter[0], counter[1], counter[2]);
+    int j = 0, s = 0;
+    //inserting numbers
+    for (j = 0; j < counter[0]; ++j)
+        array[j] = 0;
+    for (s = j; j < s + counter[1]; ++j)
+        array[j] = 1;
+    for (s = j; j < s + counter[2]; ++j)
+        array[j] = 2;
 
-        }
-        printf("\n");
-    }
+    for (int i = 0; i < size; ++i)
+        printf("%d\n", array[i]);
 }
-void cleanup(HN *array, int *size) {
-    qsort(array, (size_t) *size, sizeof(HN), compare);
-//    for (int k = 0; k < *size; ++k)
-//        printf("%s : %llu\n", array[k].name, array[k].hash);
-//
-//    printf("\n");
-    bool flag = 0;
-    //looking for repeating elements
-    for (int k = 0; k < *size; ++k) {
-        if (array[k].hash == array[k + 1].hash) {
-            flag = 1;
-            if (array[k].next == NULL) {
-                array[k].next = (struct hn *) malloc(sizeof(HN));
-                //memcpy
-                strcpy(array[k].next->name, array[k + 1].name);
-                array[k].next->hash = array[k + 1].hash;
-                array[k].next->next = NULL;
-                array[k + 1].name[0] = '\t';
-                //special symbol to detect repeating elements
-            } else {
-                array[k].next->next = (struct hn *) malloc(sizeof(HN));
-                //memcpy
-                strcpy(array[k].next->next->name, array[k + 1].name);
-                array[k].next->next->hash = array[k + 1].hash;
-                array[k + 1].name[0] = '\t';
-                //special symbol to detect repeating elements
-            }
-        }
-    }
-    if (flag) {
-        //handling repeating elements
-        for (int k = 0; k < *size; ++k) {
-            if (array[k].name[0] == '\t') {
-                //swapping useless element with last defined
-                swap_HN(&array[k], &array[*size - 1]);
-                --(*size);
-            }
-        }
-        qsort(array, (size_t) *size, sizeof(HN), compare);
-    }
-}
+
 int main() {
-    FILE *fp;
-    fp = fopen("in.txt", "r");
-    if (fp == NULL) {
-        perror("Error while opening the file.\n");
-        exit(EXIT_FAILURE);
-    }
-    HN class[50];
-    int i = 0, size_of_array = 0;
-    char ch, t_name[25];
+    int array[SIZE];
+    for (int i = 0; i < SIZE; ++i)
+        array[i] = rand()%3;
 
-    //start of input
-    while ((ch = fgetc(fp)) != EOF) {
-        if (ch != '\n')
-            t_name[i++] = ch;
-        else {
-            t_name[i] = '\0';
-            i = 0;
-            strcpy(class[size_of_array].name, t_name);
-            class[size_of_array].hash = hashing(t_name);
-            class[size_of_array].next = NULL;
-            memset(t_name, 0, strlen(t_name));  //reusing the same string
-            ++size_of_array;
-        }
-    }
-    t_name[i] = '\0';
-    strcpy(class[size_of_array].name, t_name);
-    class[size_of_array].hash = hashing(t_name);
-    class[size_of_array++].next = NULL;
-    fclose(fp);
-    //end of input
-
-    cleanup(class, &size_of_array); //size of array may change
-
-    print_hn(class, size_of_array);
+    countSort(array, SIZE);
 }
